de4c53da120edc0540941211b2a12247bb135e30
ricsimarta <ricsimarta@gmail.com>
2025-02-10 20:56:38 +0100

    monday done

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
 
 const loadEvent = async () => {
   const rootElement = document.querySelector("#root");
-  console.log(rootElement);
+  /* rootElement.insertAdjacentHTML("beforeend", `
+    <header>
+      <button>Home</button>
+      <button>Monsters</button>
+      <button>Abilites</button>
+    </header>
+  `); */
 
-  const response = await fetch('https://www.dnd5eapi.co/api/monsters');
-  console.log(response);
+  const apiUrl = 'https://www.dnd5eapi.co';
+
+  const response = await fetch(apiUrl + '/api/monsters');
   const data = await response.json();
-  console.log(data);
+  const { results } = data;
+
+  /* const [first, second, third, fourth, fifth, ...rest] = results;
+  const monstersData = [first, second, third, fourth, fifth];
+  console.log(monstersData);
+  console.log(rest); */
+
+  for (let i = 0; i < 30; i++) {
+    const element = results[i];
+
+    const monsterResponse = await fetch(apiUrl + element.url);
+    const monsterData = await monsterResponse.json();
+    console.log(monsterData);
+
+    const monsterCardHtml = `
+      <div class="card">
+        <h3>#${i + 1}</h3>
+        <h2>${monsterData.name}</h2>
+        <img src=${apiUrl + monsterData.image} />
+        <p>${monsterData.languages}</p>
+        <div class="stats">
+          <span stat-name="intelligence">${monsterData.intelligence}</span>
+          <span stat-name="strength">${monsterData.strength}</span>
+          <span stat-name="charisma">${monsterData.charisma}</span>
+          <span stat-name="dexterity">${monsterData.dexterity}</span>
+          <span stat-name="wisdom">${monsterData.wisdom}</span>
+          <span stat-name="constitution">${monsterData.constitution}</span>
+        </div>
+      </div>
+    `;
+
+    rootElement.insertAdjacentHTML('beforeend', monsterCardHtml);
+  }
 }
 
 window.addEventListener('load', loadEvent);
